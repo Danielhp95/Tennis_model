b8ad60f029643085a436498c26664b25a1b53772
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-04-14 14:52:40 +0000

    Finished first implementation of tennis match simulator

--- omalley_simulator.py
+++ omalley_simulator.py
@@ -17,14 +17,15 @@ def simulate_match(mean_a, mean_b, variance_a, variance_b):
             if is_TB_game(game_score):
                 p = omalley.TB(mean_a, mean_b)
             else:
-                p = omalley.G(serve_p_a) if player_a_serves else omalley.G(serve_p_b)
+                p = omalley.G(serve_p_a) if player_a_serves else omalley.G(1-serve_p_b)
 
             u = np.random.uniform(0,1)
-            score[0] += 1 if u <= p else 0
-            score[1] += 1 if u > p else 0
+            game_score[0] += 1 if u <= p else 0
+            game_score[1] += 1 if u > p else 0
 
             # Change of serve
             player_a_serves = not player_a_serves
+        # Set finishes, update score 
         if game_score[0] > game_score[1]:
             score[0] += 1
         else:
@@ -32,16 +33,21 @@ def simulate_match(mean_a, mean_b, variance_a, variance_b):
     return score
 
 
-
-
+# Technical debt: check form M3 AND M5
 def is_match_over(score):
-    return False
+    return score[0] >= 3 or score[1] >= 3
 
+# Technical debt: take into account different tournaments
 def is_set_over(game_score):
-    return False
+    # Normal win
+    is_over   = game_score[0] == 6 and game_score[1] <= 4
+    is_over_2 = game_score[1] == 6 and game_score[0] <= 4
+    # Tiebreak win
+    is_over_3 = game_score[0] == 7 or game_score[1] == 7 
+    return is_over or is_over_2 or is_over_3 
 
 def is_TB_game(game_score):
-    return False
+    return game_score[0] == 6 and game_score[1] == 6
 
 def batch_simulations(num_simulations, mean_a, mean_b,
                       variance_a, variance_b):
