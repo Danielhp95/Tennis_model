2ddbad3f0f43fd94a960be470251a69b73a70f72
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-04-14 08:52:35 +0000

    began (untested) implementation of tennis match simulation

--- omalley_simulator.py
+++ omalley_simulator.py
@@ -5,9 +5,43 @@ import numpy as np
 # each simulation.
 
 # Use fixed mean and variance from each player.
-def simulate_match(winning_probability):
-    u = numpy.random.uniform(0,1)
-    return 1 if u <= winning_probability else 0
+def simulate_match(mean_a, mean_b, variance_a, variance_b):
+    score = [0,0] # Player's score
+    player_a_serves = True
+    while not is_match_over(score):
+        game_score = [0,0]
+        while not is_set_over(game_score):
+            p = None
+            serve_p_a = mean_a
+            serve_p_b = mean_b
+            if is_TB_game(game_score):
+                p = omalley.TB(mean_a, mean_b)
+            else:
+                p = omalley.G(serve_p_a) if player_a_serves else omalley.G(serve_p_b)
+
+            u = np.random.uniform(0,1)
+            score[0] += 1 if u <= p else 0
+            score[1] += 1 if u > p else 0
+
+            # Change of serve
+            player_a_serves = not player_a_serves
+        if game_score[0] > game_score[1]:
+            score[0] += 1
+        else:
+            score[1] += 1
+    return score
+
+
+
+
+def is_match_over(score):
+    return False
+
+def is_set_over(game_score):
+    return False
+
+def is_TB_game(game_score):
+    return False
 
 def batch_simulations(num_simulations, mean_a, mean_b,
                       variance_a, variance_b):
@@ -19,5 +53,5 @@ def batch_simulations(num_simulations, mean_a, mean_b,
             #do something
         elif outcome == 0: # player A loss
             #do something
-
+    return None
 
