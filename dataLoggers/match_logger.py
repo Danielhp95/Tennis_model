f7d438f68df04e1d3ebe0827a6f88c1e5ba32d49
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-04-17 12:08:42 +0000

    started implementation of match logger

---
+++ dataLoggers/match_logger.py
@@ -0,0 +1,15 @@
+"""
+    Make some documentation here
+"""
+
+class matchLogger:
+
+    # serv_distribution can be fixed, gaussian
+    # Match type: M3, M5
+    # League: ATP, WTP
+    def __init__(self, serv_dist, match_type, league):
+        self.serv_dist  = serv_dist
+        self.match_type = match_type
+        self.league     = league
+       
+
