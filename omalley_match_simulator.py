00e9e43d860930a3f4a25e31d5fe6e1d72890539
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-04-18 00:48:56 +0000

    bad commit, do not follow this practice

--- omalley_match_simulator.py
+++ omalley_match_simulator.py
@@ -7,68 +7,78 @@ import numpy as np
  each simulation.
 """
 
+class MatchSimulator(class):
 
-# Use fixed mean and variance from each player.
-def simulate_match(mean_a, mean_b, variance_a, variance_b):
-    logger = ml.matchLogger("Omalley cnst", "M3", "None")
-    score = [0,0] # Player's score
-    player_a_serves = True
-    while not is_match_over(score):
-        game_score = [0,0]
-        while not is_set_over(game_score):
-            p = None
-            serve_p_a = mean_a
-            serve_p_b = mean_b
-            if is_TB_game(game_score):
-                p = omalley.TB(mean_a, mean_b)
-            else:
-                p = omalley.G(serve_p_a) if player_a_serves else omalley.G(1-serve_p_b)
+    """
+    Params:
+        
+        model_function Probability estimation function. Takes mean of two players
 
-            game_winner = update_game_score(p, game_score )
-            logger.logGame(game_winner, score)
+    """
+    def __innit__(self, game_f=omalley.G, tie_break_f=omalley.TB):
+        self.game_f      = game_f
+        self.tie_break_f = game_f
 
-            # Change of serve
-            player_a_serves = not player_a_serves
-        # Set finishes, update score 
-        set_winner = update_set_score(game_score, score)
-        logger.logSet(set_winner, game_score)
-    return score
+    # Use fixed mean and variance from each player.
+    def simulate_match(mean_a, mean_b, variance_a, variance_b):
+        logger = ml.matchLogger("Omalley cnst", "M3", "None")
+        score = [0,0] # Player's score
+        player_a_serves = True
+        while not is_match_over(score):
+            game_score = [0,0]
+            while not is_set_over(game_score):
+                p = None
+                serve_p_a = mean_a
+                serve_p_b = mean_b
+                if is_TB_game(game_score):
+                    p = tie_break_f(mean_a, mean_b)
+                else:
+                    p = game_f(serve_p_a) if player_a_serves else omalley.G(1-serve_p_b)
 
+                game_winner = update_game_score(p, game_score )
+                logger.logGame(game_winner, score)
 
-# Technical debt: check form M3 AND M5
-def is_match_over(score):
-    return score[0] >= 3 or score[1] >= 3
+                # Change of serve
+                player_a_serves = not player_a_serves
+            # Set finishes, update score 
+            set_winner = update_set_score(game_score, score)
+            logger.logSet(set_winner, game_score)
+        return score
 
-# Technical debt: take into account different tournaments
-def is_set_over(game_score):
-    # Normal win
-    is_over   = game_score[0] == 6 and game_score[1] <= 4
-    is_over_2 = game_score[1] == 6 and game_score[0] <= 4
-    # Tiebreak win
-    is_over_3 = game_score[0] == 7 or game_score[1] == 7 
-    return is_over or is_over_2 or is_over_3 
+    # Technical debt: check form M3 AND M5
+    def is_match_over(score):
+        return score[0] >= 3 or score[1] >= 3
 
-def is_TB_game(game_score):
-    return game_score[0] == 6 and game_score[1] == 6
+    # Technical debt: take into account different tournaments
+    def is_set_over(game_score):
+        # Normal win
+        is_over   = game_score[0] == 6 and game_score[1] <= 4
+        is_over_2 = game_score[1] == 6 and game_score[0] <= 4
+        # Tiebreak win
+        is_over_3 = game_score[0] == 7 or game_score[1] == 7 
+        return is_over or is_over_2 or is_over_3 
 
-# Might wanna add probability as a meassure
-def update_game_score(score):
-    winner = None
-    u = np.random.uniform(0,1)
-    if u <= p:
-        winner = 0
-        game_score[0] += 1
-    elif u > p:
-        winner = 1
-        game_score[1] += 1
-    return winner
+    def is_TB_game(game_score):
+        return game_score[0] == 6 and game_score[1] == 6
 
-def update_set_score(game_score, score):
-    winner = None
-    if game_score[0] > game_score[1]:
-        winner = 0
-        score[0] += 1
-    else:
-        winner = 1
-        score[1] += 1
-    return winner
+    # Might wanna add probability as a meassure
+    def update_game_score(score):
+        winner = None
+        u = np.random.uniform(0,1)
+        if u <= p:
+            winner = 0
+            game_score[0] += 1
+        elif u > p:
+            winner = 1
+            game_score[1] += 1
+        return winner
+
+    def update_set_score(game_score, score):
+        winner = None
+        if game_score[0] > game_score[1]:
+            winner = 0
+            score[0] += 1
+        else:
+            winner = 1
+            score[1] += 1
+        return winner
