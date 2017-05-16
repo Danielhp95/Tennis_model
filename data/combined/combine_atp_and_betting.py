4081dcb4bf559b23303828bce1067f156832234c
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-05-16 10:25:50 +0000

    changed combine table script to work on single years

--- data/combined/combine_atp_and_betting.py
+++ data/combined/combine_atp_and_betting.py
@@ -8,20 +8,23 @@ from daos.decorators import atp_to_betting as dec
 
 years = range(2007, 2018)
 
-def combine_tables():
-    for year in years:
-        print("Year " + str(year))
-        atp1, wta1 = atpdao.read_by_date(year, latest=year)
+def combine_tables(year):
+    print("Year " + str(year))
+    atp1, wta1 = atpdao.read_by_date(year, latest=year)
 
-        atp1 = dec.change_name_atp_to_betting(dec.change_date_atp_to_betting(atp1))
-        wta1 = dec.change_name_atp_to_betting(dec.change_date_atp_to_betting(wta1))
+    atp1 = dec.change_name_atp_to_betting(dec.change_date_atp_to_betting(atp1))
+    wta1 = dec.change_name_atp_to_betting(dec.change_date_atp_to_betting(wta1))
 
-        atp2, wta2 = betdao.read_by_date(earliest=year, latest=year)
+    atp2, wta2 = betdao.read_by_date(earliest=year, latest=year)
 
-        atp_combined = dec.join_atp_and_bet_tables(atp1, atp2)
-        wta_combined = dec.join_atp_and_bet_tables(wta1, wta2)
-        
-        atp_combined.to_csv(str(year) + '.csv')
-        wta_combined.to_csv(str(year) + 'w.csv')
+    print(atp2.columns.values)
+    atp_combined = dec.join_atp_and_bet_tables(atp1, atp2)
+    wta_combined = dec.join_atp_and_bet_tables(wta1, wta2)
 
-combine_tables()
+    atp_combined = pd.DataFrame(zip([1],[1]))
+    wta_combined = pd.DataFrame(zip([1],[1]))
+    
+    atp_combined.to_csv(str(year) + '.csv')
+    wta_combined.to_csv(str(year) + 'w.csv')
+
+combine_tables(int(sys.argv[1]))
