6ba73ef4df079f9babee346e5c6037135b5c168d
Ubuntu <dh1213@TennisModel.oekdt50ydcquvp1mzkoywpwjye.zx.internal.cloudapp.net>
2017-06-12 21:50:34 +0000

    Removed common opponent from package due to the fact that omalley formulae is not written in python, will be recovered later from previous commits

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo apt install python-pytest
 sudo apt install python-pip
 pip install -r requirements.txt
 
+# CURRENT VERSION DOES NOT CONTAIN O_MALLEY equations
 cd src/omalley/
 bash "swig_wrap.sh"
 cd ../../
